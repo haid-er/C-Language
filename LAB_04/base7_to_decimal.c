2d851ced1fce5643336f3830089d0bc7ef86eb8b
haid-er <malikhaiderali33@gmail.com>
2022-09-10 00:44:55 +0500

    rare changing

--- LAB_04/base7_to_decimal.c
+++ LAB_04/base7_to_decimal.c
@@ -8,16 +8,16 @@ int main()
 	int number = 0;
 	int digit = '0';
 	// Taking inputs from user
-	for (int i = 1; digit != -1; i = i * 8)
+	for (int i = 1; digit != -1; i = i * 7)
 	{
 		printf("Enter Base-7 digit : ");
 		scanf("%d", &digit);
-		if ((digit < 0 || digit > 7) && digit != -1)
+		if ((digit < 0 || digit > 6) && digit != -1)
 		{
 			printf("Invalid Character Next \n");
 			continue;
 		}
-		if(digit>=0 && digit <=7)
+		if(digit>=0 && digit <=6)
 			number = number + (digit * i);
 
 	}
