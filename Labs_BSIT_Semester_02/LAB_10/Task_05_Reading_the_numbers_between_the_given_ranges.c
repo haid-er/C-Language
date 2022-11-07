ff3340292e12abf5db5cc5135b36102076071eb9
haid-er <malikhaiderali33@gmail.com>
2022-11-07 21:23:25 +0500

    Updating task 5 of lab 10

--- Labs_BSIT_Semester_02/LAB_10/Task_05_Reading_the_numbers_between_the_given_ranges.c
+++ Labs_BSIT_Semester_02/LAB_10/Task_05_Reading_the_numbers_between_the_given_ranges.c
@@ -5,6 +5,7 @@ ReadThemAll().The function will assign the array address to a pointer called p a
 numbers in all the slots between these two numbers.The ReadThemAll() function should not
 use brackets, [] but only the asterisk operator,* .When the function is done, have main() print
 out the entire array.
+if the given array is a = { 1, 2, 3, 4, 5, 6, 7} and given numbers are 2 and 5 then output should be 3, 4, 5.
 */
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
@@ -15,50 +16,75 @@ out the entire array.
 void randInput(int* p);
 void readThemAll(int* p,int a,int b);
 void display(const int* p);
+int numberExistence(int* arr, int num, int* index);
 int main()
 {
-	srand(time(0));
-
 	// Declaration of array
 
 	int tArray[SIZE];
 	int num1, num2;
-	num1 = num2 = 0;
+	int numberExistenceFlag = 0;
+	int indexStart, indexEnd;
+	num1 = num2 = indexStart = indexEnd = 0;
 
-	// Taking inputs from user
+	// Initializing array with random number and printing it
 
 	randInput(tArray);
-	printf("Enter starting number : ");
-	scanf("%d", &num1);
-	printf("Enter Ending Number : ");
-	scanf("%d", &num2);
+	printf("Initialized Array : \n\n");
+	display(tArray);
+	printf("\n\n");
+
+	// Taking inputs from user
+	
+	do
+	{
+		do
+		{
+			printf("Enter starting number : ");
+			scanf("%d", &num1);
+			numberExistenceFlag = numberExistence(tArray, num1, &indexStart);
+			if (numberExistenceFlag == 0)
+				printf("Number does not exist in Array ! ");
+		} while (numberExistenceFlag == 0);
+		numberExistenceFlag = 0;
+		do
+		{
+			printf("Enter Ending number : ");
+			scanf("%d", &num2);
+			numberExistenceFlag = numberExistence(tArray, num2, &indexEnd);
+			if (numberExistenceFlag == 0)
+				printf("Number does not exist in Array ! ");
+		} while (numberExistenceFlag == 0);
+		if (indexStart > indexEnd)
+			printf("Starting and Ending number are not given correctly and may be start number is after the ending number ! Please enter any other input .\n");
+	} while (indexStart > indexEnd);
+
+	
 
 	// Processing
 
 	printf("Numbers between the Given range : \n");
-	readThemAll(tArray, num1, num2);
+	readThemAll(tArray, indexStart + 1, indexEnd); // added one in index start to not show the starting number
 
 
 	// Displaying Output
-
-	printf("Whole Array : \n");
-	display(tArray);
 
 	return 0;
 
 }
 void randInput(int* p)
 {
+	srand(time(0));
 	for (int i = 0; i < SIZE; i++)
 	{
-		*(p + i) = rand() % 100;
+		*(p + i) = rand();
 	}
-	*(p + SIZE) = '\0';
+	*(p + SIZE - 1) = '\0';
 }
 void display(const int* p)
 {
 	int i = 0;
-	while(*(p+i) != 0)
+	while (*(p + i) != 0)
 	{
 		printf("%d\t", *(p + i));
 		i++;
@@ -66,10 +92,22 @@ void display(const int* p)
 }
 void readThemAll(int* p, int a, int b)
 {
-	for (int i = 0; *(p + i - 1) != 0; i++)
+	for (int i = a; i < b; i++)
 	{
-		if (*(p + i) >= a && *(p + i) <= b)
-			printf("%d\t", *(p + i));
+		printf("%d\t", *(p + i));
 	}
 	printf("\n");
+}
+int numberExistence(int* arr, int num, int* index)
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (*(arr + i) == num)
+		{
+			*index = i;
+			return 1;
+		}
+
+	}
+	return 0;
 }
